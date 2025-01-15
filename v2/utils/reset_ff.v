896c16ebf296bffda7c9440584ee12b92b79d316
BalasaiAnish <ponnaluribalasai@gmail.com>
2025-01-15 17:50:20 +0530

    added

--- v2/utils/reset_ff.v
+++ v2/utils/reset_ff.v
@@ -23,13 +23,21 @@
 module ff #(
     parameter DATA_WIDTH=32
     )(
-    input clk,
+    input clk,rst,en,
     input [DATA_WIDTH-1:0] din,
     output reg [DATA_WIDTH-1:0] dout
 );
 
-always @(posedge clk) begin
-    dout <= din;
+always @(posedge clk or posedge rst) begin
+    if(rst) begin
+        dout <= 0;
+    end
+
+    else begin
+    	// active low enable
+    	if(!en)
+        	dout <= din;
+    end
 end
 
 endmodule
